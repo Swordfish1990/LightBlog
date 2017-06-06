58195a9fbf580300d5f35d5b6250deb52e88c6b4
moonlight0925 <moonlight0925@hotmail.com>
2017-06-06 21:13:59 +0800

    11111

--- LightBlog/LightBlog/__init__.py
+++ LightBlog/LightBlog/__init__.py
@@ -13,6 +13,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     db.init_app(app)
+    db.app=app
     app.register_blueprint(front)
     app.register_blueprint(plugin)
     return app
